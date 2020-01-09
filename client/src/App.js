f08b568a00c8099b4eb18f793682b9555b61b379
grobelDev <grobelDev@gmail.com>
2020-01-09 13:50:05 -0800

    testing cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+// function buttonPress() {
+//   console.log('button was pressed');
+// }
+
+async function pingServer() {
+  // // let serverUrl = 'https://server-nch7pipeyq-uc.a.run.app';
+  // let serverUrl2 = 'http://localhost:8080';
+  // let response = await fetch(serverUrl2);
+  // // response.json();
+  // if (response.ok) {
+  //   // const _response = await response.json();
+  //   console.log(response);
+  //   // return response2.accountId;
+
+  fetch('https://server-nch7pipeyq-uc.a.run.app')
+    .then(response => {
+      return response.json();
+    })
+    .then(myJson => {
+      console.log(myJson);
+    });
+}
+
 export default function App() {
   return (
     <div>
@@ -7,7 +30,10 @@ export default function App() {
         League of Comrades
       </div>
       <div className='flex justify-center'>
-        <button className='px-4 py-2 font-normal text-white bg-blue-500 rounded hover:bg-blue-700'>
+        <button
+          className='px-4 py-2 font-normal text-white bg-blue-500 rounded hover:bg-blue-700'
+          onClick={() => pingServer()}
+        >
           Ping Server
         </button>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// app.use(cors());
 app.use(bodyParser.json());
 
 // Express route handlers
-
 app.get('/', (req, res) => {
-  res.send('Greetings from the Server');
+  res.send({ express: 'Hello From Express' });
 });
 
 app.listen(8080, err => {
