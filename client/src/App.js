b773ea0ae9f100fc9f5f156a57cd46cc6ea9f327
grobelDev <grobelDev@gmail.com>
2020-01-13 13:00:06 -0800

    implemented header

--- client/src/App.js
+++ client/src/App.js
@@ -77,12 +77,7 @@ const MessageWrapper = styled.div`
 `;
 
 const MobileWarning = () => {
-  return (
-    <MessageWrapper>
-      These demos should be viewed on a mobile device, an emulator or the mobile
-      view in your devtools.
-    </MessageWrapper>
-  );
+  return <MessageWrapper>Currently only optimized for mobile!</MessageWrapper>;
 };
 
 const routes = [
@@ -96,6 +91,57 @@ const routes = [
   // { path: "/photo-grid", component: PhotoGrid, title: "Photo Grid" },
   // { path: "/notification", component: Notification, title: "Notification" }
 ];
+
+const CustomHeader = () => {
+  return (
+    <div id='header'>
+      <div className='fixed inset-x-0 top-0 z-10 flex items-center h-16 bg-white border-b border-gray-200'>
+        <div className='relative w-full px-6 mx-auto max-w-screen-xl'>
+          <div className='flex items-center -mx-6'>
+            <div className='pl-6 pr-6 lg:w-1/4 xl:w-1/5 lg:pr-8'>
+              <div className='flex items-center'>
+                <a href='/summoner-list' className='block lg:mr-4'>
+                  LoC
+                </a>
+              </div>
+            </div>
+            <div className='flex flex-grow bg-gray lg:w-3/4 xl:w-4/5'>
+              <div className='w-full lg:px-6 xl:w-3/4 xl:px-12'>
+                <div className='relative'>
+                  <div>
+                    <input className='block w-full py-2 pl-10 pr-4 leading-normal placeholder-gray-600 bg-gray-200 border border-transparent rounded-lg appearance-none transition focus:outline-0 focus:bg-white focus:border-gray-300 ds-input'></input>
+                  </div>
+                  <div className='absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none'>
+                    <svg
+                      class='fill-current pointer-events-none text-gray-600 w-4 h-4'
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 20 20'
+                    >
+                      <path d='M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z'></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              type='button'
+              id='sidebar-open'
+              class='flex px-6 items-center lg:hidden text-gray-500 focus:outline-none focus:text-gray-700'
+            >
+              <svg
+                class='fill-current w-4 h-4'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+              >
+                <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
+              </svg>
+            </button>{' '}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -125,9 +171,14 @@ function App() {
           {routes.map(r => {
             const Component = r.component;
             return (
-              <Route path={r.path} key={r.path}>
-                <Component />
-              </Route>
+              <div>
+                <Route path={r.path} key={r.path}>
+                  <CustomHeader></CustomHeader>
+                  <div className='mt-16'>
+                    <Component />
+                  </div>
+                </Route>
+              </div>
             );
           })}
         </Switch>
