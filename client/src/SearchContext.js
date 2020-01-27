fb4300bd1bc0dea5a32c88805f50720baf06d151
grobelDev <grobelDev@gmail.com>
2020-01-27 01:21:14 -0800

    added google analytics and some cleanup

--- client/src/SearchContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SearchContext = React.createContext({ name: '', region: '' });
-
-export const SearchProvider = SearchContext.Provider;
-export const SearchConsumer = SearchContext.Consumer;
-export default SearchContext;
