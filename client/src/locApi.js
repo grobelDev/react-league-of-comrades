696bc40ff231bbaa1696de72c9680c474b33c32e
grobelDev <56764278+grobelDev@users.noreply.github.com>
2020-02-20 05:57:54 -0800

    changed server url

--- client/src/locApi.js
+++ client/src/locApi.js
@@ -75,7 +75,7 @@ function wrapPromise(promise) {
 
 function fetchComrades(userName, userRegion) {
   return new Promise(function(resolve, reject) {
-    let url = 'https://server-nch7pipeyq-uc.a.run.app';
+    let url = 'https://whenshouldileave-server-zxc6fpw5uq-uc.a.run.app';
     let testUrl = 'http://localhost:8080';
 
     // let currentUrl = url;
